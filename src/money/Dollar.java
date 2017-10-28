36a651002df756d9b54c1090bcec81ec451c7751
fujiwaraakinori <foxyfox64@gmail.com>
2017-10-28 21:15:14 +0900

    p38

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -5,8 +5,6 @@ package money;
  */
 public class Dollar extends Money {
 
-    private int amount;
-
     public Dollar(int amount) {
         this.amount = amount;
 
@@ -16,10 +14,10 @@ public class Dollar extends Money {
         return new Dollar(amount * multiplier);
     }
 
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
-    }
+//    public boolean equals(Object object) {
+//        Money money = (Money) object;
+//        return amount == money.amount;
+//    }
 
 
 }
--- src/money/Money.java
+++ src/money/Money.java
@@ -4,9 +4,15 @@ package money;
  * Created by fujiwaraakinori on 2017/10/28.
  */
 public class Money {
+    protected int amount;
 
     public Money() {
 
+    }
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount;
     }
 
 }
