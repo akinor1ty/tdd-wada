b8611a8cd4bdd0a155b80d1282bcf8c9d7a0d6d7
fujiwaraakinori <foxyfox64@gmail.com>
2017-10-28 21:19:17 +0900

    p39

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -7,17 +7,10 @@ public class Dollar extends Money {
 
     public Dollar(int amount) {
         this.amount = amount;
-
     }
 
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
-
-//    public boolean equals(Object object) {
-//        Money money = (Money) object;
-//        return amount == money.amount;
-//    }
-
 
 }
